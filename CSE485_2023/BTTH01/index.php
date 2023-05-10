dac34a2b4a00078f0f0f5a675e7cb19fe600ef45
tranqynhanhhh <84771483+tranqynhanhhh@users.noreply.github.com>
2023-05-10 23:23:27 +0700

    Add files via upload

---
+++ CSE485_2023/BTTH01/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Student Information Form</title>
+</head>
+<body>
+	<h2>Student Information Form</h2>
+	<form action="student.php" method="post">
+		<label for="id">ID:</label>
+		<input type="text" id="id" name="id"><br>
+        <label for="name">Name:</label>
+		<input type="text" id="name" name="name"><br>
+        <label for="age">Age:</label>
+		<input type="text" id="age" name="age"><br>
+        
+        <input type="submit" value="Submit">
+	</form>
+</body>
+</html>
