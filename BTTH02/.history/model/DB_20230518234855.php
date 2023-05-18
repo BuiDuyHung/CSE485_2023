01fdb0179991d3c0201799b67bcd2fd08e8f72af
virtual-port <hungb.z98@gmail.com>
2023-05-18 23:56:14 +0700

    update file DB.php

---
+++ BTTH02/.history/model/DB_20230518234855.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost"; // Tên máy chủ CSDL
+$username = "root"; // Tên người dùng CSDL
+$password = ""; // Mật khẩu CSDL
+$database = "db_attendance"; // Tên CSDL
+
+// Tạo kết nối
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Kiểm tra kết nối
+if ($conn->connect_error) {
+    die("Kết nối CSDL thất bại: " . $conn->connect_error);
+}
---
+++ BTTH02/.history/model/DB_20230518235141.php
@@ -0,0 +1,18 @@
+<?php
+
+//Kết nối cơ sở dữ liệu
+function connect()
+{
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "db_attendance";
+
+    $conn = new mysqli($host, $username, $password, $database);
+
+    if ($conn->connect_error) {
+        die("Kết nối không thành công: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
