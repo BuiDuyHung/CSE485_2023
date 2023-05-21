fcd67f63846a4677c0fe0a685b810d0ea312ed55
quanghuy1234567 <nguyenquanghuy27042002@gmail.com>
2023-05-21 17:05:15 +0700

    Update admin.php and model

--- BTTH02/model/student.php
+++ BTTH02/model/student.php
@@ -2,9 +2,12 @@
     class student extends DB{
         public function __construct(){parent::__construct();}
 
-        public function getAll(){
-            $sql = "select id, fullName from students";
-            $stmt = parent::pdo($sql);
+        public function getStudentByAttendanceId($attID){
+            $sql = "select attendancerecords.student_id, fullName, email, status
+                    from attendancerecords 
+                    inner join students ON students.ID = attendancerecords.student_id
+                    where attendance_id = :att_id ";
+            $stmt = parent::pdo($sql,[':att_id'=>$attID]);
             return $stmt->fetchAll();
         }
 
